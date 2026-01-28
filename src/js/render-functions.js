7f47a1edf784d489025de679361d75a62411a1e1
Erik <erik@Eriks-MacBook-Pro.local>
2026-01-28 21:40:37 +0200

    homework

---
+++ src/js/render-functions.js
@@ -0,0 +1,79 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export default {
+  createGallery: images => {
+    const iamgesMarkup = images.map(img => {
+      return `
+            <li class="gallery-item">
+            <a class="gallery-link" href="${img.largeImageURL}">
+                <img class="gallery-img" src="${img.webformatURL}" alt="${img.tags}">
+                <ul class="gallery-text-container">
+                <li class="gallery-text">
+                    <div class="gallery-title">
+                    Likes
+                    </div>
+                    <div class="gallery-sub-title">
+                    ${img.likes}
+                    </div>
+                </li>
+                <li class="gallery-text">
+                    <div class="gallery-title">
+                    Views
+                    </div>
+                    <div class="gallery-sub-title">
+                    ${img.views}
+                    </div>
+                </li>
+                <li class="gallery-text">
+                    <div class="gallery-title">
+                    Comments
+                    </div>
+                    <div class="gallery-sub-title">
+                    ${img.comments}
+                    </div>
+                </li>
+                <li class="gallery-text">
+                    <div class="gallery-title">
+                    Downloads
+                    </div>
+                    <div class="gallery-sub-title">
+                    ${img.downloads}
+                    </div>
+                </li>
+                </ul>
+            </a>
+            </li>
+        `;
+    });
+    const gallery = document.querySelector('.gallery-container');
+    gallery.innerHTML = iamgesMarkup.join('');
+
+    const galleryItems = document.querySelectorAll('.gallery-item');
+
+    galleryItems.forEach(galleryItem => {
+      galleryItem.addEventListener('click', e => {
+        e.preventDefault();
+
+        const simpleLightbox = new SimpleLightbox('.gallery-link', {
+          captionsData: 'alt',
+          captionDelay: 250,
+        });
+
+        simpleLightbox.refresh();
+      });
+    });
+  },
+  clearGallery: () => {
+    const gallery = document.querySelector('.gallery-container');
+    gallery.innerHTML = '';
+  },
+  showLoader: () => {
+    const gallery = document.querySelector('.gallery-container');
+    gallery.innerHTML = '<span class="loader"></span>';
+  },
+  hideLoader: () => {
+    const gallery = document.querySelector('.gallery-container');
+    gallery.innerHTML = '';
+  },
+};
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import iziToast from 'izitoast';
+
+import pixabayApi from './js/pixabay-api';
+import renderFunctions from './js/render-functions';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+const searchButton = document.querySelector('.form-button');
+searchButton.addEventListener('click', () => {
+  renderFunctions.clearGallery();
+  renderFunctions.showLoader();
+  const searchInput = document.querySelector('.form-input');
+  const searchText = searchInput.value;
+  pixabayApi
+    .getImagesByQuery(searchText)
+    .then(response => {
+      renderFunctions.hideLoader();
+
+      if (response.data.hits.length === 0) {
+        iziToast.error({
+          message: `Sorry, there are no images matching your search query. Please try again!`,
+        });
+      } else {
+        renderFunctions.createGallery(response.data.hits);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
